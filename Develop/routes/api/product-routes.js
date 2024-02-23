7771be430e497b7d385cf1a91df666264bae7e1b
akent <alexanderkent099@gmail.com>
2024-02-23 17:09:56 -0500

    Implemented route to delete a product by ID

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -85,9 +85,24 @@ router.put('/:id', async (req, res) => {
   }
 });  
 
+// delete product
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Product deleted!' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
